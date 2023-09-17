78352c05f999e382f79a95ae6a28f44bfe35cac8
migueelzz <miguellemes005@gmail.com>
2023-09-17 18:33:44 -0300

    config cors api

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import { fastify } from 'fastify'
+import cors from '@fastify/cors'  
 import { DatabasePostgres } from './database-postgres.js'
 
 const server = fastify()
+
+server.register(cors, {
+  origin: "*",
+})
 
 const database = new DatabasePostgres()
 
